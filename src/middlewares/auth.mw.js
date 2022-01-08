4af334ad49ced680bc82ab14491ddbd7b4ef45fe
Prasanna Mangoli <Prasanna Mangoli>
2022-01-08 15:48:20 +0530

    eslintnig auth middleware

--- src/middlewares/auth.mw.js
+++ src/middlewares/auth.mw.js
@@ -1,21 +1,18 @@
 const jwt = require('jsonwebtoken');
 
+const { sendError } = require('@magcentre/response-helper');
 const config = require('../configuration/config');
 
-const { sendError } = require('@magcentre/response-helper');
-
 module.exports.jwt = function (req, res, next) {
+  if (!req.headers.authorization) return sendError('Authentication required', res, 401, req);
 
-    if (!req.headers.authorization) return sendError('Authentication required', res, 401, req);
+  jwt.verify(req.headers.authorization.replace('Bearer ', ''), config.jwt.secret, (error, decoded, info) => {
+    if (error) return sendError('Error in JWT authentication process', res, 500, req);
 
-    jwt.verify(req.headers.authorization.replace('Bearer ',''), config.jwt.secret, function (error, decoded, info) {
+    if (!decoded) return sendError(`Authentication failed,: ${info}`, res, 401, req);
 
-        if (error) return sendError('Error in JWT authentication process', res, 500, req);
+    req.auth = decoded;
 
-        if (!decoded) return sendError('Authentication failed,: ' + info, res, 401, req);
-
-        req.auth = decoded;
-
-        return next();
-    });
+    return next();
+  });
 };
