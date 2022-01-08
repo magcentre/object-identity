e840a6b08270d1d6ff68cd06cd89acc78200b8de
Prasanna Mangoli <Prasanna Mangoli>
2022-01-08 15:48:03 +0530

    eslinting applied

--- src/models/token.model.js
+++ src/models/token.model.js
@@ -1,53 +1,51 @@
-const mongoose = require('@magcentre/mongoose-helper').mongoose;
+const { mongoose } = require('@magcentre/mongoose-helper');
 
 const tokenTypes = {
-    ACCESS: 'access',
-    REFRESH: 'refresh',
-    RESET_PASSWORD: 'resetPassword',
-    VERIFY_EMAIL: 'verifyEmail',
+  ACCESS: 'access',
+  REFRESH: 'refresh',
+  RESET_PASSWORD: 'resetPassword',
+  VERIFY_EMAIL: 'verifyEmail',
 };
 
 const tokenSchema = mongoose.Schema(
-    {
-        token: {
-            type: String,
-            required: true,
-            index: true,
-        },
-        user: {
-            type: mongoose.SchemaTypes.ObjectId,
-            ref: 'User',
-            required: true,
-        },
-        type: {
-            type: String,
-            enum: [tokenTypes.REFRESH, tokenTypes.RESET_PASSWORD, tokenTypes.VERIFY_EMAIL],
-            required: true,
-        },
-        expires: {
-            type: Date,
-            required: true,
-        },
-        blacklisted: {
-            type: Boolean,
-            default: false,
-        },
+  {
+    token: {
+      type: String,
+      required: true,
+      index: true,
     },
-    {
-        timestamps: true,
-        versionKey: false
-    }
+    user: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: [tokenTypes.REFRESH, tokenTypes.RESET_PASSWORD, tokenTypes.VERIFY_EMAIL],
+      required: true,
+    },
+    expires: {
+      type: Date,
+      required: true,
+    },
+    blacklisted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
 );
 
 /**
  * Create and save access token
  * @param {object} tokenConfig - javscript object with all required parameters to create token
  * @param {function(err, model)} cb - Callback function
- * @returns {Promise<Token>} - 
+ * @returns {Promise<Token>} -
  */
-tokenSchema.statics.createToken = function (tokenConfig, cb) {
-    return this.create(tokenConfig, cb);
-}
+tokenSchema.statics.createToken = (tokenConfig, cb) => this.create(tokenConfig, cb);
 
 /**
  * @typedef Token
@@ -55,6 +53,6 @@ tokenSchema.statics.createToken = function (tokenConfig, cb) {
 const Token = mongoose.model('Token', tokenSchema);
 
 module.exports = {
-    model: Token,
-    types: tokenTypes
-}
+  model: Token,
+  types: tokenTypes,
+};
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,65 +1,65 @@
+/* eslint-disable func-names */
 const { mongoose } = require('@magcentre/mongoose-helper');
 
 const bcrypt = require('bcryptjs');
 
 const userTypes = {
-    USER: 'access',
-    ADMIN: 'refresh'
+  USER: 'access',
+  ADMIN: 'refresh',
 };
 
 const userSchema = mongoose.Schema(
-    {
-        firstName: {
-            type: String,
-            required: true,
-            trim: true,
-        },
-        lastName: {
-            type: String,
-            required: true,
-            trim: true,
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-            trim: true,
-            lowercase: true,
-            validate(value) {
-                var e = String(value)
-                    .toLowerCase()
-                    .match(
-                        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-                    );
-
-                if (!e) {
-                    throw { statuCode: 400, message: 'Invalid email' };
-                }
-            },
-        },
-        password: {
-            type: String,
-            required: true,
-            trim: true,
-            minlength: 8,
-            validate(value) {
-                if (!value.match(/\d/) || !value.match(/[a-zA-Z]/)) {
-                    throw { statuCode: 400, message: 'Password must contain at least one letter and one number' };
-                }
-            },
-        },
-        role: {
-            type: String,
-            enum: [userTypes.ADMIN, userTypes.USER],
-            default: userTypes.USER,
-        },
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
     },
-    {
-        timestamps: true,
-        versionKey: false
-    }
-);
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      validate(value) {
+        const e = String(value)
+          .toLowerCase()
+          .match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+          );
 
+        if (!e) {
+          throw Error({ statuCode: 400, message: 'Invalid email' });
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 8,
+      validate(value) {
+        if (!value.match(/\d/) || !value.match(/[a-zA-Z]/)) {
+          throw Error({ statuCode: 400, message: 'Password must contain at least one letter and one number' });
+        }
+      },
+    },
+    role: {
+      type: String,
+      enum: [userTypes.ADMIN, userTypes.USER],
+      default: userTypes.USER,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
 
 /**
  * Check if email is taken
@@ -68,7 +68,7 @@ const userSchema = mongoose.Schema(
  * @returns {Promise<boolean>}
  */
 userSchema.statics.isEmailTaken = function (email, excludeUserId) {
-    return this.findOne({ email, _id: { $ne: excludeUserId } });
+  return this.findOne({ email, _id: { $ne: excludeUserId } });
 };
 
 /**
@@ -77,21 +77,21 @@ userSchema.statics.isEmailTaken = function (email, excludeUserId) {
  * @returns {Promise<Object>}
  */
 userSchema.methods.isPasswordMatch = function (password) {
-    const user = this;
-    return new Promise((resolve, reject) => {
-        bcrypt.compare(password, user.password, (err, isValid) => {
-            if (err) return reject(err);
-            return resolve({ match: isValid, ...user.toObject() });
-        });
+  const user = this;
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(password, user.password, (err, isValid) => {
+      if (err) return reject(err);
+      return resolve({ match: isValid, ...user.toObject() });
     });
+  });
 };
 
 userSchema.pre('save', async function (next) {
-    const user = this;
-    if (user.isModified('password')) {
-        user.password = await bcrypt.hash(user.password, 8);
-    }
-    next();
+  const user = this;
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+  next();
 });
 
 /**
@@ -100,6 +100,6 @@ userSchema.pre('save', async function (next) {
 const User = mongoose.model('Users', userSchema);
 
 module.exports = {
-    model: User,
-    types: userTypes
+  model: User,
+  types: userTypes,
 };
