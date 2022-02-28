165a2578aeec9216b9baf26ec372ebabe29e9416
Prasanna Mangoli <Prasanna Mangoli>
2022-02-28 22:28:51 +0530

    middleware await to promise

--- src/middlewares/auth.mw.js
+++ src/middlewares/auth.mw.js
@@ -4,17 +4,15 @@ const utils = require('@magcentre/api-utils');
 
 const config = require('../configuration/config');
 
-module.exports.jwt = async (req, res, next) => {
+module.exports.jwt = (req, res, next) => {
   if (!req.headers.authorization) return sendError({ message: 'Authentication required' }, res, 401, req);
 
   const token = req.headers.authorization.replace('Bearer ', '');
-
-  try {
-    const decoded = await utils.verifyJWTToken(token, config.jwt.secret);
-    req.auth = decoded;
-    return next();
-  }
-  catch (err) {
-    return sendError(err, res, err.statusCode || 500, req);
-  }
+  utils.verifyJWTToken(token, config.jwt.secret)
+    .then((decoded) => {
+      req.auth = decoded;
+      return next();
+    })
+    .catch((err) => sendError(err, res, err.statusCode || 500, req));
+  
 };
