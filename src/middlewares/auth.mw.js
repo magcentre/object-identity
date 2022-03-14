3eb26042e617710ae1e28e97ead1d9f231b10cff
Prasanna Mangoli <mangoli.prasanna05@gmail.com>
2022-03-14 11:48:26 +0530

    Feature/user management (#20)
    * verify bucket exists during email login with mobile
    
    * account verification for email registration
    
    * role in token
    
    * otp auth: OTP expiry
    
    * refresh token fix, verify bucket during email login
    
    * verified and block senario
    
    * swagger reset
    
    * user list with admin scope api
    
    * user path api
    
    * mongo-express
    
    * error handeling
    
    * swagger config update
    
    Co-authored-by: Prasanna <prasanna@Prasanna-MacBook-Air.local>

--- src/middlewares/auth.mw.js
+++ src/middlewares/auth.mw.js
@@ -6,11 +6,14 @@ const config = require('../configuration/config');
 
 module.exports.jwt = (req, res, next) => {
   if (!req.headers.authorization) return sendError({ message: 'Authentication required' }, res, 401, req);
-
+  const operations = req.swagger.operation['x-security-scopes'];
   const token = req.headers.authorization.replace('Bearer ', '');
-  utils.verifyJWTToken(token, config.jwt.secret)
+  return utils.verifyJWTToken(token, config.jwt.secret)
     .then((decoded) => {
       req.auth = decoded;
+      if (operations && decoded.role !== operations) {
+        return sendError({ message: 'Forbidden' }, res, 403, req);
+      }
       return next();
     })
     .catch((err) => sendError(err, res, err.statusCode || 500, req));
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,3 @@
-/* eslint-disable func-names */
 const { mongoose } = require('@magcentre/mongoose-helper');
 const { getRichError } = require('@magcentre/response-helper');
 
@@ -239,6 +238,26 @@ UserAccount.setOTP = (mobile, otp, expiry) => UserAccount.findOneAndUpdate({ mob
 UserAccount.getUserByMobile = (mobile) => UserAccount.findOne({ mobile })
   .catch((err) => {
     throw getRichError('System', 'error while finding user with mobile', { err, mobile }, err, 'error', null);
+  });
+
+/**
+ * Find user by mobile number
+ * @param {String} mobile search user by mobile number
+ * @returns Promise
+ */
+UserAccount.getUsersList = () => UserAccount.find({}, { otp: 0, otpExpiry: 0, password: 0 })
+  .catch((err) => {
+    throw getRichError('System', 'error while fetching all users list user with mobile', {}, err, 'error', null);
+  });
+
+/**
+* Find user by mobile number
+* @param {String} mobile search user by mobile number
+* @returns Promise
+*/
+UserAccount.updateUserById = (userId, properties) => UserAccount.findByIdAndUpdate(userId, properties)
+  .catch((err) => {
+    throw getRichError('System', 'error while updating the user details', { userId, properties }, err, 'error', null);
   });
 
 module.exports = {
